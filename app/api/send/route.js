119a0211d7f803fb5d68a0f389d097fd60b96852
Shirin <shirinsadeghi@live.com>
2024-03-21 02:36:34 -0700

    ..

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,29 +1,29 @@
-import { Resend } from 'resend';
-import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
+// import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const {body} = await req.json();
-  const {email, subject, message} = body
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: ['shirinsadeghi@live.com', email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting me!</p>
-          <p>{message}</p>
-        </>
-      )
+// export async function POST(req, res) {
+//   const {body} = await req.json();
+//   const {email, subject, message} = body
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: ['shirinsadeghi@live.com', email],
+//       subject: subject,
+//       react: (
+//         <>
+//           <h1>{subject}</h1>
+//           <p>Thank you for contacting me!</p>
+//           <p>{message}</p>
+//         </>
+//       )
       
-    });
+//     });
 
-    return NextResponseResponse.json(data);
-  } catch (error) {
-    return NextResponseResponse.json({ error });
-  }
-}
+//     return NextResponseResponse.json(data);
+//   } catch (error) {
+//     return NextResponseResponse.json({ error });
+//   }
+// }
