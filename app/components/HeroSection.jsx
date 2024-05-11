f9556698d358af0bb1ae3e54f4f607de66489fd3
Shirin <shirinsadeghi@live.com>
2024-05-11 16:37:24 -0700

    fix bug for menu overlay component to redirect to correct section using scrollIntoView

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -46,7 +46,6 @@ const scrollToEmailSection = () => {
                     <Link href={"/"}>
                     <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'
                         onClick={scrollToEmailSection}
-                        
                     >Hire Me
                         
                     </button>
--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import NavLink from './NavLink'
+import React from 'react';
+import NavLink from './NavLink';
 
-const MenuOverlay = ({links}) => {
-  console.log('links', links)
+const MenuOverlay = ({ links }) => {
+  const handleScrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <ul className='flex flex-col py-4 items-center'>
-        {links.map((link, index) => (
-            <li key={index}>
-            <NavLink href={link.id} title={link.title} />
-            </li>
-        ))}
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink
+            onClick={() => handleScrollToSection(link.id)}
+            href={`#${link.id}`}
+            title={link.title}
+          />
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default MenuOverlay
+export default MenuOverlay;
